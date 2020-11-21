5f0286a09c80e4a2a40d8485f23012a1c3784453
Oscar Higgott <oscarhiggott@users.noreply.github.com>
2020-11-21 13:48:41 +0000

    Lemon is now the default matching library

--- src/pymatching/bindings.cpp
+++ src/pymatching/bindings.cpp
@@ -12,63 +12,63 @@ namespace py = pybind11;
 using namespace pybind11::literals;
 
 PYBIND11_MODULE(_cpp_mwpm, m) {
-    py::class_<PerfectMatching>(m, "PerfectMatching")
-    .def(py::init<int, int>(), "nodeNum"_a, "edgeNumMax"_a)
-    .def("add_edge", &PerfectMatching::AddEdge, 
-         "i"_a, "j"_a, "cost"_a)
-    .def("solve", &PerfectMatching::Solve, "finish"_a=true)
-    .def("get_match", &PerfectMatching::GetMatch, "i"_a)
-    .def("get_solution", &PerfectMatching::GetSolution, "e"_a)
-    .def_readwrite("options", &PerfectMatching::options);
+     py::class_<PerfectMatching>(m, "PerfectMatching")
+     .def(py::init<int, int>(), "nodeNum"_a, "edgeNumMax"_a)
+     .def("add_edge", &PerfectMatching::AddEdge, 
+          "i"_a, "j"_a, "cost"_a)
+     .def("solve", &PerfectMatching::Solve, "finish"_a=true)
+     .def("get_match", &PerfectMatching::GetMatch, "i"_a)
+     .def("get_solution", &PerfectMatching::GetSolution, "e"_a)
+     .def_readwrite("options", &PerfectMatching::options);
 
-    py::class_<PerfectMatching::Options>(m, "Options")
-    .def(py::init<>())
-    .def_readwrite("fractional_jumpstart", &PerfectMatching::Options::fractional_jumpstart)
-    .def_readwrite("dual_greedy_update_option", &PerfectMatching::Options::dual_greedy_update_option)
-    .def_readwrite("dual_LP_threshold", &PerfectMatching::Options::dual_LP_threshold)
-    .def_readwrite("update_duals_before", &PerfectMatching::Options::update_duals_before)
-    .def_readwrite("update_duals_after", &PerfectMatching::Options::update_duals_after)
-    .def_readwrite("single_tree_threshold", &PerfectMatching::Options::single_tree_threshold)
-    .def_readwrite("verbose", &PerfectMatching::Options::verbose);
+     py::class_<PerfectMatching::Options>(m, "Options")
+     .def(py::init<>())
+     .def_readwrite("fractional_jumpstart", &PerfectMatching::Options::fractional_jumpstart)
+     .def_readwrite("dual_greedy_update_option", &PerfectMatching::Options::dual_greedy_update_option)
+     .def_readwrite("dual_LP_threshold", &PerfectMatching::Options::dual_LP_threshold)
+     .def_readwrite("update_duals_before", &PerfectMatching::Options::update_duals_before)
+     .def_readwrite("update_duals_after", &PerfectMatching::Options::update_duals_after)
+     .def_readwrite("single_tree_threshold", &PerfectMatching::Options::single_tree_threshold)
+     .def_readwrite("verbose", &PerfectMatching::Options::verbose);
 
-    py::class_<IStabiliserGraph>(m, "IStabiliserGraph")
-    .def("distance", &IStabiliserGraph::Distance, "node1"_a, "node2"_a)
-    .def("space_time_distance", &IStabiliserGraph::SpaceTimeDistance, "node1"_a, "node2"_a)
-    .def("shortest_path", &IStabiliserGraph::ShortestPath, "node1"_a, "node2"_a)
-    .def("space_time_shortest_path", &IStabiliserGraph::SpaceTimeShortestPath, "node1"_a, "node2"_a)
-    .def("qubit_ids", &IStabiliserGraph::QubitIDs, "node1"_a, "node2"_a)
-    .def("get_num_qubits", &IStabiliserGraph::GetNumQubits)
-    .def("get_num_stabilisers", &IStabiliserGraph::GetNumStabilisers)
-    .def("get_num_edges", &IStabiliserGraph::GetNumEdges)
-    .def("compute_all_pairs_shortest_paths", &IStabiliserGraph::ComputeAllPairsShortestPaths)
-    .def("has_computed_all_pairs_shortest_paths", &IStabiliserGraph::HasComputedAllPairsShortestPaths)
-    .def("get_num_connected_components", &IStabiliserGraph::GetNumConnectedComponents);
+     py::class_<IStabiliserGraph>(m, "IStabiliserGraph")
+     .def("distance", &IStabiliserGraph::Distance, "node1"_a, "node2"_a)
+     .def("space_time_distance", &IStabiliserGraph::SpaceTimeDistance, "node1"_a, "node2"_a)
+     .def("shortest_path", &IStabiliserGraph::ShortestPath, "node1"_a, "node2"_a)
+     .def("space_time_shortest_path", &IStabiliserGraph::SpaceTimeShortestPath, "node1"_a, "node2"_a)
+     .def("qubit_ids", &IStabiliserGraph::QubitIDs, "node1"_a, "node2"_a)
+     .def("get_num_qubits", &IStabiliserGraph::GetNumQubits)
+     .def("get_num_stabilisers", &IStabiliserGraph::GetNumStabilisers)
+     .def("get_num_edges", &IStabiliserGraph::GetNumEdges)
+     .def("compute_all_pairs_shortest_paths", &IStabiliserGraph::ComputeAllPairsShortestPaths)
+     .def("has_computed_all_pairs_shortest_paths", &IStabiliserGraph::HasComputedAllPairsShortestPaths)
+     .def("get_num_connected_components", &IStabiliserGraph::GetNumConnectedComponents);
 
-    py::class_<WeightedStabiliserGraph, IStabiliserGraph>(m, "WeightedStabiliserGraph")
-    .def(py::init<int, std::vector<int>&>(), "num_stabilisers"_a, "boundary"_a)
-    .def_readwrite("all_predecessors", &WeightedStabiliserGraph::all_predecessors)
-    .def_readwrite("all_distances", &WeightedStabiliserGraph::all_distances)
-    .def_readwrite("all_edges_have_error_probabilities", 
+     py::class_<WeightedStabiliserGraph, IStabiliserGraph>(m, "WeightedStabiliserGraph")
+     .def(py::init<int, std::vector<int>&>(), "num_stabilisers"_a, "boundary"_a)
+     .def_readwrite("all_predecessors", &WeightedStabiliserGraph::all_predecessors)
+     .def_readwrite("all_distances", &WeightedStabiliserGraph::all_distances)
+     .def_readwrite("all_edges_have_error_probabilities", 
                     &WeightedStabiliserGraph::all_edges_have_error_probabilities)
-    .def("add_edge", &WeightedStabiliserGraph::AddEdge, "node1"_a, "node2"_a, "qubit_ids"_a, 
-         "weight"_a, "error_probability"_a=-1.0, "has_error_probability"_a=false)
-    .def("add_noise", &WeightedStabiliserGraph::AddNoise)
-    .def("get_boundary", &WeightedStabiliserGraph::GetBoundary)
-    .def("set_boundary", &WeightedStabiliserGraph::SetBoundary, "boundary"_a)
-    .def("get_nearest_neighbours", &WeightedStabiliserGraph::GetNearestNeighbours,
-         "source"_a, "num_neighbours"_a, "defect_id"_a)
-    .def("get_path", &WeightedStabiliserGraph::GetPath, "source"_a, "target"_a);
+     .def("add_edge", &WeightedStabiliserGraph::AddEdge, "node1"_a, "node2"_a, "qubit_ids"_a, 
+          "weight"_a, "error_probability"_a=-1.0, "has_error_probability"_a=false)
+     .def("add_noise", &WeightedStabiliserGraph::AddNoise)
+     .def("get_boundary", &WeightedStabiliserGraph::GetBoundary)
+     .def("set_boundary", &WeightedStabiliserGraph::SetBoundary, "boundary"_a)
+     .def("get_nearest_neighbours", &WeightedStabiliserGraph::GetNearestNeighbours,
+          "source"_a, "num_neighbours"_a, "defect_id"_a)
+     .def("get_path", &WeightedStabiliserGraph::GetPath, "source"_a, "target"_a);
 
-    m.def("bv_decode", &Decode, "sg"_a, "defects"_a);
-    m.def("bv_decode_match_neighbourhood", &DecodeMatchNeighbourhood,
+     m.def("bv_decode", &Decode, "sg"_a, "defects"_a);
+     m.def("bv_decode_match_neighbourhood", &DecodeMatchNeighbourhood,
           "sg"_a ,"defects"_a, "num_neighbours"_a);
-    m.def("randomize", &randomize);
-    m.def("set_seed", &set_seed, "s"_a);
-    m.def("rand_float", &rand_float, "from"_a, "to"_a);
+     m.def("randomize", &randomize);
+     m.def("set_seed", &set_seed, "s"_a);
+     m.def("rand_float", &rand_float, "from"_a, "to"_a);
 
-    m.def("decode_match_neighbourhood", &BoostDecodeMatchNeighbourhood, "sg"_a ,"defects"_a, "num_neighbours"_a);
-     m.def("decode", &BoostDecode, "sg"_a, "defects"_a);
+     m.def("boost_decode_match_neighbourhood", &BoostDecodeMatchNeighbourhood, "sg"_a ,"defects"_a, "num_neighbours"_a);
+     m.def("boost_decode", &BoostDecode, "sg"_a, "defects"_a);
 
-    m.def("lemon_decode_match_neighbourhood", &LemonDecodeMatchNeighbourhood);
-     m.def("lemon_decode", &LemonDecode);
+     m.def("decode_match_neighbourhood", &LemonDecodeMatchNeighbourhood);
+     m.def("decode", &LemonDecode);
 }
