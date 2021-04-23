26eda76f1f745eb118295c603437515d4e00939a
Oscar Higgott <oscarhiggott@users.noreply.github.com>
2021-04-23 17:54:15 +0100

    Allow python 3 before 3.6

--- setup.py
+++ setup.py
@@ -133,6 +133,6 @@ setup(
     ],
     long_description=long_description,
     long_description_content_type='text/markdown',
-    python_requires='>=3.6',
+    python_requires='>=3',
     zip_safe=False,
 )
