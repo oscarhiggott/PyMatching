56f64c3bac0fb5918d3e8cc99926a1ca700d6eb2
Oscar Higgott <oscarhiggott@users.noreply.github.com>
2022-08-12 17:21:46 -0700

    bucket queue

--- bucket_queue.h
+++ bucket_queue.h
@@ -6,11 +6,28 @@
 
 namespace pm {
 
-    template <pm::weight_int NUM_BUCKETS>
+    template <pm::weight_int NUM_QUEUE_BUCKETS>
     class bucket_queue {
     public:
-        std::vector<TentativeEvent*> bucket_queue[NUM_BUCKETS];
+        std::vector<TentativeEvent> buckets_array[NUM_QUEUE_BUCKETS];
+        pm::weight_int current_bucket;
+        bucket_queue();
+        template< class... Args >
+        void emplace( Args&&... args );
+        void push(const TentativeEvent& event);
+        const TentativeEvent& top() const;
+        void pop();
+        bool empty() const;
     };
+
+    template<pm::weight_int NUM_QUEUE_BUCKETS>
+    bucket_queue<NUM_QUEUE_BUCKETS>::bucket_queue() : current_bucket(0) {}
+
+    template<pm::weight_int NUM_QUEUE_BUCKETS>
+    void bucket_queue<NUM_QUEUE_BUCKETS>::push(const TentativeEvent &event) {
+        pm::weight_int bucket = event.time % NUM_QUEUE_BUCKETS;
+        buckets_array[bucket].push_back(event);
+    }
 
 }
 
