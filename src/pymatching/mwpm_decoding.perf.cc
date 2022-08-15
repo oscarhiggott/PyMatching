c245a747f56ac1394f26452c3ffe3afec1f3a2d8
Craig Gidney <craiggidney@google.com>
2022-08-14 23:03:54 -0700

    Add rounds to perf

--- src/pymatching/mwpm_decoding.perf.cc
+++ src/pymatching/mwpm_decoding.perf.cc
@@ -29,8 +29,41 @@ std::pair<stim::DetectorErrorModel, std::vector<stim::SparseShot>> generate_data
     return {dem, shots};
 }
 
+BENCHMARK(Decode_surface_r5_d5_p1000) {
+    size_t rounds = 5;
+    auto data = generate_data(5, rounds, 0.001, 1024);
+    const auto &dem = data.first;
+    const auto &shots = data.second;
+
+    size_t num_buckets = 1024;
+    auto mwpm = pm::detector_error_model_to_mwpm(dem, num_buckets);
+
+    size_t num_dets = 0;
+    for (const auto &shot : shots) {
+        num_dets += shot.hits.size();
+    }
+
+    size_t num_mistakes = 0;
+    benchmark_go([&]() {
+        for (const auto &shot : shots) {
+            auto res = pm::decode_detection_events(mwpm, shot.hits);
+            if (shot.obs_mask != res.obs_mask) {
+                num_mistakes++;
+            }
+        }
+    })
+        .goal_millis(1)
+        .show_rate("dets", (double)num_dets)
+        .show_rate("layers", (double)rounds * shots.size())
+        .show_rate("shots", (double)shots.size());
+    if (num_mistakes == shots.size()) {
+        std::cerr << "data dependence";
+    }
+}
+
 BENCHMARK(Decode_surface_r11_d11_p100) {
-    auto data = generate_data(11, 11, 0.01, 128);
+    size_t rounds = 11;
+    auto data = generate_data(11, rounds, 0.01, 128);
     const auto &dem = data.first;
     const auto &shots = data.second;
 
@@ -53,6 +86,7 @@ BENCHMARK(Decode_surface_r11_d11_p100) {
     })
         .goal_millis(30)
         .show_rate("dets", (double)num_dets)
+        .show_rate("layers", (double)rounds * shots.size())
         .show_rate("shots", (double)shots.size());
     if (num_mistakes == shots.size()) {
         std::cerr << "data dependence";
@@ -60,7 +94,8 @@ BENCHMARK(Decode_surface_r11_d11_p100) {
 }
 
 BENCHMARK(Decode_surface_r11_d11_p1000) {
-    auto data = generate_data(11, 11, 0.001, 512);
+    size_t rounds = 11;
+    auto data = generate_data(11, rounds, 0.001, 512);
     const auto &dem = data.first;
     const auto &shots = data.second;
 
@@ -83,6 +118,7 @@ BENCHMARK(Decode_surface_r11_d11_p1000) {
     })
         .goal_millis(5)
         .show_rate("dets", (double)num_dets)
+        .show_rate("layers", (double)rounds * shots.size())
         .show_rate("shots", (double)shots.size());
     if (num_mistakes == shots.size()) {
         std::cerr << "data dependence";
@@ -90,7 +126,8 @@ BENCHMARK(Decode_surface_r11_d11_p1000) {
 }
 
 BENCHMARK(Decode_surface_r11_d11_p10000) {
-    auto data = generate_data(11, 11, 0.0001, 256);
+    size_t rounds = 11;
+    auto data = generate_data(11, rounds, 0.0001, 256);
     const auto &dem = data.first;
     const auto &shots = data.second;
 
@@ -113,6 +150,7 @@ BENCHMARK(Decode_surface_r11_d11_p10000) {
     })
         .goal_micros(180)
         .show_rate("dets", (double)num_dets)
+        .show_rate("layers", (double)rounds * shots.size())
         .show_rate("shots", (double)shots.size());
     if (num_mistakes == shots.size()) {
         std::cerr << "data dependence";
@@ -120,7 +158,8 @@ BENCHMARK(Decode_surface_r11_d11_p10000) {
 }
 
 BENCHMARK(Decode_surface_r11_d11_p100000) {
-    auto data = generate_data(11, 11, 0.00001, 1024);
+    size_t rounds = 11;
+    auto data = generate_data(11, rounds, 0.00001, 1024);
     const auto &dem = data.first;
     const auto &shots = data.second;
 
@@ -143,6 +182,7 @@ BENCHMARK(Decode_surface_r11_d11_p100000) {
     })
         .goal_micros(80)
         .show_rate("dets", (double)num_dets)
+        .show_rate("layers", (double)rounds * shots.size())
         .show_rate("shots", (double)shots.size());
     if (num_mistakes == shots.size()) {
         std::cerr << "data dependence";
@@ -150,7 +190,8 @@ BENCHMARK(Decode_surface_r11_d11_p100000) {
 }
 
 BENCHMARK(Decode_surface_r21_d21_p100) {
-    auto data = generate_data(21, 21, 0.01, 8);
+    size_t rounds = 21;
+    auto data = generate_data(21, rounds, 0.01, 8);
     const auto &dem = data.first;
     const auto &shots = data.second;
 
@@ -173,6 +214,7 @@ BENCHMARK(Decode_surface_r21_d21_p100) {
     })
         .goal_millis(20)
         .show_rate("dets", (double)num_dets)
+        .show_rate("layers", (double)rounds * shots.size())
         .show_rate("shots", (double)shots.size());
     if (num_mistakes == 0) {
         std::cerr << "data dependence";
@@ -180,7 +222,8 @@ BENCHMARK(Decode_surface_r21_d21_p100) {
 }
 
 BENCHMARK(Decode_surface_r21_d21_p1000) {
-    auto data = generate_data(21, 21, 0.001, 256);
+    size_t rounds = 21;
+    auto data = generate_data(21, rounds, 0.001, 256);
     const auto &dem = data.first;
     const auto &shots = data.second;
 
@@ -203,6 +246,7 @@ BENCHMARK(Decode_surface_r21_d21_p1000) {
     })
         .goal_millis(17)
         .show_rate("dets", (double)num_dets)
+        .show_rate("layers", (double)rounds * shots.size())
         .show_rate("shots", (double)shots.size());
     if (num_mistakes == shots.size()) {
         std::cerr << "data dependence";
@@ -210,7 +254,8 @@ BENCHMARK(Decode_surface_r21_d21_p1000) {
 }
 
 BENCHMARK(Decode_surface_r21_d21_p10000) {
-    auto data = generate_data(21, 21, 0.0001, 512);
+    size_t rounds = 21;
+    auto data = generate_data(21, rounds, 0.0001, 512);
     const auto &dem = data.first;
     const auto &shots = data.second;
 
@@ -233,6 +278,7 @@ BENCHMARK(Decode_surface_r21_d21_p10000) {
     })
         .goal_millis(3)
         .show_rate("dets", (double)num_dets)
+        .show_rate("layers", (double)rounds * shots.size())
         .show_rate("shots", (double)shots.size());
     if (num_mistakes == shots.size()) {
         std::cerr << "data dependence";
@@ -240,7 +286,8 @@ BENCHMARK(Decode_surface_r21_d21_p10000) {
 }
 
 BENCHMARK(Decode_surface_r21_d21_p100000) {
-    auto data = generate_data(21, 21, 0.00001, 512);
+    size_t rounds = 21;
+    auto data = generate_data(21, rounds, 0.00001, 512);
     const auto &dem = data.first;
     const auto &shots = data.second;
 
@@ -263,6 +310,7 @@ BENCHMARK(Decode_surface_r21_d21_p100000) {
     })
         .goal_micros(250)
         .show_rate("dets", (double)num_dets)
+        .show_rate("layers", (double)rounds * shots.size())
         .show_rate("shots", (double)shots.size());
     if (num_mistakes == shots.size()) {
         std::cerr << "data dependence";
