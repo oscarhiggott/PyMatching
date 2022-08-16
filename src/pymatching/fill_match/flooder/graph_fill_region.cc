c7f6bb1bb7801038e7f3fc601139a7c66295837f
Craig Gidney <craiggidney@google.com>
2022-08-16 15:32:28 -0700

    de-pm

--- src/pymatching/fill_match/flooder/graph_fill_region.cc
+++ src/pymatching/fill_match/flooder/graph_fill_region.cc
@@ -3,11 +3,13 @@
 #include "pymatching/fill_match/flooder/graph.h"
 #include "pymatching/fill_match/flooder_matcher_interop/varying.h"
 
-pm::GraphFillRegion::GraphFillRegion()
+using namespace pm;
+
+GraphFillRegion::GraphFillRegion()
     : blossom_parent(nullptr), alt_tree_node(nullptr), radius((0 << 2) + 1), shrink_event_tracker() {
 }
 
-pm::GraphFillRegion *pm::GraphFillRegion::top_region() const {
+GraphFillRegion *GraphFillRegion::top_region() const {
     auto current = const_cast<GraphFillRegion *>(this);
     while (current->blossom_parent) {
         current = current->blossom_parent;
@@ -15,7 +17,7 @@ pm::GraphFillRegion *pm::GraphFillRegion::top_region() const {
     return current;
 }
 
-bool pm::GraphFillRegion::tree_equal(const pm::GraphFillRegion &other) const {
+bool GraphFillRegion::tree_equal(const GraphFillRegion &other) const {
     if (alt_tree_node != other.alt_tree_node || radius != other.radius ||
         blossom_children.size() != other.blossom_children.size() || shell_area != other.shell_area) {
         return false;
@@ -31,20 +33,20 @@ bool pm::GraphFillRegion::tree_equal(const pm::GraphFillRegion &other) const {
     return true;
 }
 
-bool pm::GraphFillRegion::operator==(const pm::GraphFillRegion &rhs) const {
+bool GraphFillRegion::operator==(const GraphFillRegion &rhs) const {
     return tree_equal(rhs);
 }
 
-bool pm::GraphFillRegion::operator!=(const pm::GraphFillRegion &rhs) const {
+bool GraphFillRegion::operator!=(const GraphFillRegion &rhs) const {
     return !(rhs == *this);
 }
 
-void pm::GraphFillRegion::add_match(pm::GraphFillRegion *region, const pm::CompressedEdge &edge) {
+void GraphFillRegion::add_match(GraphFillRegion *region, const CompressedEdge &edge) {
     match = Match{region, edge};
     region->match = Match{this, edge.reversed()};
 }
 
-void pm::GraphFillRegion::cleanup_shell_area() {
+void GraphFillRegion::cleanup_shell_area() {
     for (auto &detector_node : shell_area) {
         detector_node->reset();
     }
