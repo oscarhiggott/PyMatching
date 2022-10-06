9081b5b33adb2bba4a200a7347738bdf85f03aa2
Oscar Higgott <oscarhiggott@users.noreply.github.com>
2022-10-07 00:21:01 +0100

    add_noise test added

--- src/pymatching/matching_test.py
+++ src/pymatching/matching_test.py
@@ -694,3 +694,44 @@ def test_negative_weight_edge_returned():
                 (1, 2, {'fault_ids': {0}, 'weight': 0.5, 'error_probability': 0.3}),
                 (2, 3, {'fault_ids': {1, 2}, 'weight': -0.5, 'error_probability': 0.7})]
     assert m.edges() == expected
+
+
+def test_add_noise():
+    p = 0.1
+    N = 1000
+    std = (p * (1 - p) / N) ** 0.5
+    g = nx.Graph()
+    for i in range(N):
+        g.add_edge(i, i + 1, fault_ids=i, weight=-np.log(p), error_probability=p)
+    m = Matching(g)
+    for i in range(5):
+        noise, syndrome = m.add_noise()
+        assert (sum(syndrome) % 2) == 0
+        assert (p - 5 * std) * N < sum(noise) < (p + 5 * std) * N
+        for i in range(1, N - 1):
+            assert syndrome[i] == (noise[i - 1] + noise[i]) % 2
+
+
+def test_add_noise_with_boundary():
+    g = nx.Graph()
+    for i in range(11):
+        g.add_edge(i, i + 1, fault_ids=i, error_probability=(i + 1) % 2)
+    for i in range(5, 12):
+        g.nodes()[i]['is_boundary'] = True
+    m = Matching(g)
+    noise, syndrome = m.add_noise()
+    assert sum(syndrome) == 5
+    assert np.array_equal(noise, (np.arange(11) + 1) % 2)
+    assert m.boundary == set(range(5, 12))
+    assert np.array_equal(
+        syndrome,
+        np.array([1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0])
+    )
+
+
+def test_add_noise_without_error_probabilities_returns_none():
+    m = Matching(csr_matrix(np.array([[1, 1, 0], [0, 1, 1]])))
+    assert m.add_noise() is None
+    m = Matching(csr_matrix(np.array([[1, 1, 0], [0, 1, 1]])),
+                 error_probabilities=np.array([0.5, 0.7, -0.1]))
+    assert m.add_noise() is None
