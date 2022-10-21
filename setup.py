5ccdf4c72b29b37d198830f2f559b76676f5fc95
Oscar Higgott <oscarhiggott@users.noreply.github.com>
2022-10-21 09:55:00 +0100

    setup --build for ms

--- setup.py
+++ setup.py
@@ -54,8 +54,8 @@ class CMakeBuild(build_ext):
         if "CMAKE_ARGS" in os.environ:
             cmake_args += [item for item in os.environ["CMAKE_ARGS"].split(" ") if item]
 
-        # In this example, we pass in the version to C++. You might not need to.
-        cmake_args += [f"-DPYMATCHING_VERSION_INFO={self.distribution.get_version()}"]
+        # # In this example, we pass in the version to C++. You might not need to.
+        # cmake_args += [f"-DPYMATCHING_VERSION_INFO={self.distribution.get_version()}"]
 
         if self.compiler.compiler_type != "msvc":
             # Using Ninja-build since it a) is available as a wheel and b)
